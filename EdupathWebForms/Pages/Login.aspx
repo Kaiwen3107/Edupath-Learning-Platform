1ed211b1db53ce43e154f86bd1adc29352471363
kaiwen0731 <kaiwen0731@gmail.com>
2025-04-24 10:17:49 +0800

    Upload login.apsx

---
+++ EdupathWebForms/Pages/Login.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="EdupathWebForms.Pages.Login" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Edupath - Login</title>
+    <link href="Assets/styles.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="login-container">
+            <h2>Edupath Learning Platform</h2>
+            <div class="form-group">
+                <asp:Label ID="lblUsername" runat="server" Text="Username:"></asp:Label>
+                <asp:TextBox ID="txtUsername" runat="server" CssClass="form-control"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvUsername" runat="server" 
+                    ControlToValidate="txtUsername" ErrorMessage="Username is required" 
+                    ForeColor="Red"></asp:RequiredFieldValidator>
+            </div>
+            <div class="form-group">
+                <asp:Label ID="lblPassword" runat="server" Text="Password:"></asp:Label>
+                <asp:TextBox ID="txtPassword" runat="server" TextMode="Password" CssClass="form-control"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvPassword" runat="server" 
+                    ControlToValidate="txtPassword" ErrorMessage="Password is required" 
+                    ForeColor="Red"></asp:RequiredFieldValidator>
+            </div>
+            <div class="form-group">
+                <asp:Button ID="btnLogin" runat="server" Text="Login" OnClick="btnLogin_Click" CssClass="btn-primary" />
+            </div>
+            <div class="form-group">
+                <asp:Label ID="lblMessage" runat="server" ForeColor="Red"></asp:Label>
+            </div>
+            <div class="form-links">
+                <a href="Register.aspx">Register new account</a> | 
+                <a href="ForgotPassword.aspx">Forgot password?</a>
+            </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ EdupathWebForms/Pages/Login.aspx.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace EdupathWebForms.Pages
+{
+    public partial class Login : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Check if user is already logged in
+            if (Session["UserID"] != null)
+            {
+                RedirectBasedOnRole();
+            }
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUsername.Text.Trim();
+            string password = txtPassword.Text;
+
+            // In a real application, you would hash the password
+            // This is simplified for demonstration
+            string connectionString = ConfigurationManager.ConnectionStrings["EdupathConnectionString"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("SELECT UserID, Username, RoleID FROM Users WHERE Username = @Username AND Password = @Password ", conn);
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Password", password); // Use hashing in real app!
+
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    // Authentication successful
+                    Session["UserID"] = reader["UserID"].ToString();
+                    Session["Username"] = reader["Username"].ToString();
+                    Session["RoleID"] = reader["RoleID"].ToString();
+
+                    RedirectBasedOnRole();
+                }
+                else
+                {
+                    // Authentication failed
+                    lblMessage.Text = "Invalid username or password.";
+                }
+                reader.Close();
+            }
+        }
+
+        private void RedirectBasedOnRole()
+        {
+            int roleID = Convert.ToInt32(Session["RoleID"]);
+            switch (roleID)
+            {
+                case 1: // Admin
+                    Response.Redirect("~/Admin/Dashboard.aspx");
+                    break;
+                case 2: // Teacher
+                    Response.Redirect("~/Teacher/Dashboard.aspx");
+                    break;
+                case 3: // Student
+                    Response.Redirect("~/Student/Dashboard.aspx");
+                    break;
+                default:
+                    Response.Redirect("~/Default.aspx");
+                    break;
+            }
+        }
+    }
+}
