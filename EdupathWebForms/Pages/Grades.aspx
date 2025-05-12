8e10c9a8de49c3911e2fde933e702b4b06226cb0
kaiwen0731 <kaiwen0731@gmail.com>
2025-05-12 22:08:54 +0800

    View Grade Page Create

---
+++ EdupathWebForms/Pages/Grades.aspx
@@ -0,0 +1,64 @@
+﻿<%@ Page Title="Grades" Language="C#" MasterPageFile="Site.master" AutoEventWireup="true" CodeBehind="Grades.aspx.cs" Inherits="EdupathWebForms.Pages.Grades" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2 class="mb-4">📊 My Grades</h2>
+
+    <h4>Assignments</h4>
+    <asp:Repeater ID="rptAssignments" runat="server">
+        <HeaderTemplate>
+            <table class="table table-bordered">
+                <thead class="table-light">
+                    <tr>
+                        <th>Title</th>
+                        <th>Course</th>
+                        <th>Submitted At</th>
+                        <th>Grade</th>
+                    </tr>
+                </thead>
+                <tbody>
+        </HeaderTemplate>
+        <ItemTemplate>
+                    <tr>
+                        <td><%# Eval("Title") %></td>
+                        <td><%# Eval("ClassName") %></td>
+                        <td><%# Eval("SubmissionAT", "{0:yyyy-MM-dd HH:mm}") %></td>
+                        <td><%# Eval("Grade") == DBNull.Value ? "Not Graded" : Eval("Grade") + " / 100" %></td>
+                    </tr>
+        </ItemTemplate>
+        <FooterTemplate>
+                </tbody>
+            </table>
+        </FooterTemplate>
+    </asp:Repeater>
+
+    <h4 class="mt-5">Quizzes</h4>
+    <asp:Repeater ID="rptQuizzes" runat="server">
+        <HeaderTemplate>
+            <table class="table table-bordered">
+                <thead class="table-light">
+                    <tr>
+                        <th>Quiz</th>
+                        <th>Course</th>
+                        <th>Attempt(s)</th>
+                        <th>Score</th>
+                        <th>Last Submitted</th>
+                    </tr>
+                </thead>
+                <tbody>
+        </HeaderTemplate>
+        <ItemTemplate>
+                    <tr>
+                        <td><%# Eval("Title") %></td>
+                        <td><%# Eval("ClassName") %></td>
+                        <td><%# Eval("AttemptHistory") %></td>
+                        <td><%# Eval("Score") + " / " + Eval("TotalMarks") %></td>
+                        <td><%# Eval("SubmittedAt", "{0:yyyy-MM-dd HH:mm}") %></td>
+                    </tr>
+        </ItemTemplate>
+        <FooterTemplate>
+                </tbody>
+            </table>
+        </FooterTemplate>
+    </asp:Repeater>
+
+</asp:Content>
---
+++ EdupathWebForms/Pages/Grades.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace EdupathWebForms.Pages
+{
+    public partial class Grades : System.Web.UI.Page
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["EdupathConnectionString"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["UserID"] == null || Session["Role"]?.ToString() != "student")
+            {
+                Response.Redirect("Login.aspx");
+                return;
+            }
+
+            if (!IsPostBack)
+            {
+                LoadAssignmentGrades();
+                LoadQuizGrades();
+            }
+        }
+
+        private void LoadAssignmentGrades()
+        {
+            int studentId = Convert.ToInt32(Session["UserID"]);
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(@"
+                    SELECT a.Title, c.ClassName, s.SubmissionAT, s.Grade
+                    FROM Submission s
+                    INNER JOIN Assignment a ON s.AssignmentID = a.AssignmentID
+                    INNER JOIN Class c ON a.ClassID = c.ClassID
+                    WHERE s.StudentID = @StudentID", conn);
+                cmd.Parameters.AddWithValue("@StudentID", studentId);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                rptAssignments.DataSource = dt;
+                rptAssignments.DataBind();
+            }
+        }
+
+        private void LoadQuizGrades()
+        {
+            int studentId = Convert.ToInt32(Session["UserID"]);
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand(@"
+                    SELECT q.Title, c.ClassName, s.Score, s.AttemptHistory, s.SubmittedAt,
+                           (SELECT COUNT(*) FROM Question WHERE QuizID = q.QuizID) AS TotalMarks
+                    FROM QuizSubmission s
+                    INNER JOIN Quiz q ON s.QuizID = q.QuizID
+                    INNER JOIN Class c ON q.ClassID = c.ClassID
+                    WHERE s.StudentID = @StudentID", conn);
+                cmd.Parameters.AddWithValue("@StudentID", studentId);
+
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                rptQuizzes.DataSource = dt;
+                rptQuizzes.DataBind();
+            }
+        }
+    }
+}
